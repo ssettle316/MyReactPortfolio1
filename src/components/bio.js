4baeb9516f7db136c6465e45345c3aaa8e16003f
Stephen Settle <ssettle316@gmail.com>
2020-07-14 18:28:03 -0400

    Portfolio update1

---
+++ src/components/bio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function bio() {
+    return (
+        <div>
+            <p>I am an Entrepreneur with a background in Accounting, Business & Tech. I am currently tuning my skills to become a full stack mobile and web developer. I am interested in building various mobile applications from the ground up. I have previously worked on the business side of many applications. I plan on combining my business and web devloping background to create projects that I am passionate about.</p>
+          <br />
+          <p>As I go through this journey of being a web & mobile developer, I am very much interested in co-oping with other like minded individuals. I plan to one day create apps that individuals can download/purchase on Google Play & Apple Store.
+          </div>
+    )
+}
